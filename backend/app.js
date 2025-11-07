94a8910407f1ec0ae05a6006463df5f3bc9bb76e
adamtorokhu <torok.adam05@gmail.com>
2025-11-07 15:18:52 +0100

    Set trust proxy for Express app

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 3000;
+app.set('trust proxy', 1);
 const mongodb = require('./services/mongodb');
 const multer  = require('multer');
 const path = require('path');
